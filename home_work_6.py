c105cf316155fa02b0f61543bd4c580994e23a45
Ser-Julia <heyjulia93@gmail.com>
2026-01-09 00:37:50 +0400

    git commit

--- home_work_6.py
+++ home_work_6.py
@@ -4,5 +4,5 @@ response = requests.post("https://playground.learnqa.ru/api/long_redirect", allo
 count_response = len(response.history)
 last_response = response
 
-print(f"Количество ридиректов: {count_response}")
+print(f"Количество редиректов: {count_response}")
 print(f"Итоговый URL: {last_response.url}")
