e6a385971dd293d951254a0746fac2f2bb8f03bf
Ser-Julia <heyjulia93@gmail.com>
2026-01-09 00:34:09 +0400

    git commit

---
+++ home_work_6.py
@@ -0,0 +1,8 @@
+import requests
+
+response = requests.post("https://playground.learnqa.ru/api/long_redirect", allow_redirects=True)
+count_response = len(response.history)
+last_response = response
+
+print(f"Количество ридиректов: {count_response}")
+print(f"Итоговый URL: {last_response.url}")
--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-from json.decoder import JSONDecodeError
+#from json.decoder import JSONDecodeError
 import requests
 
-response = requests.get("https://playground.learnqa.ru/api/get_text")
-print(response.text)
-
+response = requests.post("https://playground.learnqa.ru/api/get_301", allow_redirects=True)
+#print(response.status_code)
+#print(response.text)
+first_response = response.history[0]
+second_response = response
+print(first_response.url)
+print(second_response.url)
+'''
 try:
     parsed_response_text = response.json()
     print(parsed_response_text)
 except JSONDecodeError:
     print("Response is not JSON format")
+'''
